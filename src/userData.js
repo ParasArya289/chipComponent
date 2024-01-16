53fae38d314f11a949b30b3a01d8c3e9d2b60734
Paras Arya <parasarya289@gmail.com>
2024-01-16 23:42:17 +0530

    feat: mock data

---
+++ src/userData.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    id: 987654,
+    name: "John Doe",
+    email: "john.doe@email.com",
+    profilePic: "https://www.svgrepo.com/show/384672/account-avatar-profile-user-7.svg",
+  },
+  {
+    id: 123456,
+    name: "Jane Smith",
+    email: "jane.smith@email.com",
+    profilePic:
+      "https://www.svgrepo.com/show/384680/account-avatar-profile-user-4.svg",
+  },
+  {
+    id: 456789,
+    name: "Alex Johnson",
+    email: "alex.johnson@email.com",
+    profilePic: "https://www.svgrepo.com/show/384677/account-avatar-profile-user-12.svg",
+  },
+  {
+    id: 789012,
+    name: "Sarah Williams",
+    email: "sarah.williams@email.com",
+    profilePic:
+      "https://www.svgrepo.com/show/384682/account-avatar-profile-user-10.svg",
+  },
+  {
+    id: 345678,
+    name: "Michael Davis",
+    email: "michael.davis@email.com",
+    profilePic: "https://www.svgrepo.com/show/384669/account-avatar-profile-user-13.svg",
+  },
+];
