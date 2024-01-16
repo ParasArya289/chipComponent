07a40e13e2755165f1d2e5a8ccad935b22414773
Paras Arya <parasarya289@gmail.com>
2024-01-16 23:41:42 +0530

    feat: Input Component

--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -1,10 +1,86 @@
-import { useRef, useState } from "react";
+import { useMemo, useRef, useState } from "react";
+import { Pill } from "../Pill/Pill";
+import { UserList } from "../SuggestionBox/UserList";
 import "./Input.css";
 
-export const Input = () => {
-  const [textArray, setTextArray] = useState([]);
+export const Input = ({ data, placeholder }) => {
+  const [selectedUserArray, setSelectedUserArray] = useState([]);
+  const [usersArray, setUsersArray] = useState(data);
   const [backSpaceCount, setBackSpaceCount] = useState(0);
+  const [inputValue, setInputValue] = useState("");
+  const [suggestionBoxVisible, setSuggestionBoxVisible] = useState(false);
+
   const inputRef = useRef(null);
+  const blurTimeoutRef = useRef(null);
+
+  const handleInput = (e) => {
+    setInputValue(e.target.innerText.trim());
+  };
+
+  // filter user and sort data to closest match
+  const filteredData = useMemo(() => {
+    return usersArray
+      .filter(({ name }) => name?.toLowerCase().includes(inputValue))
+      ?.sort((a, b) => {
+        const valueA = a?.name.indexOf(inputValue);
+        const valueB = b?.name.indexOf(inputValue);
+        return valueB - valueA;
+      });
+  }, [inputValue, usersArray]);
+
+  // handle keydown event
+  const handleKey = (e) => {
+    switch (e.key) {
+      case "Enter":
+        e.preventDefault();
+        break;
+      case "Backspace":
+        if (!inputValue.length && selectedUserArray.length) {
+          if (backSpaceCount === 0) {
+            setBackSpaceCount(1);
+          } else {
+            deleteLastUser();
+            setBackSpaceCount(0);
+          }
+        }
+        break;
+      default:
+        setBackSpaceCount(0);
+        break;
+    }
+  };
+
+  //add and delete users handlers
+  const selectUser = (user) => {
+    setSelectedUserArray((prev) => [...prev, user]);
+    removeUserFromSuggestion(user?.id);
+    setInputValue("");
+    inputRef.current.innerText = "";
+  };
+
+  const deleteLastUser = () => {
+    const lastUser = selectedUserArray.at(-1);
+    addUserToSuggestion(lastUser);
+
+    setSelectedUserArray((prev) =>
+      prev.filter((_, i) => i !== prev.length - 1)
+    );
+  };
+
+  const deleteUser = (id) => {
+    const user = selectedUserArray.find((user) => user.id === id);
+    addUserToSuggestion(user);
+    setSelectedUserArray((prev) => prev.filter((user) => id !== user.id));
+  };
+
+  const removeUserFromSuggestion = (id) => {
+    setUsersArray((prev) => prev.filter((user) => id !== user.id));
+  };
+  const addUserToSuggestion = (user) => {
+    setUsersArray((prev) => [...prev, user]);
+  };
+
+  //input and suggestion box blur and focus handlers
 
   const focusInput = () => {
     if (inputRef.current) {
@@ -13,51 +89,31 @@ export const Input = () => {
     }
   };
 
-  const handleKey = (e) => {
-    if (inputRef.current) {
-      const textValue = inputRef?.current.innerText;
-      if (e.key === "Enter") {
-        if (textValue) {
-          e.preventDefault();
-          setTextArray((prev) => [...prev, textValue]);
-          inputRef.current.innerText = "";
-        }
-      }
-      if (e.key !== "Backspace") {
-        setBackSpaceCount(0);
-      }
-      if (!textValue.length && textArray.length && e.key === "Backspace") {
-        switch (backSpaceCount) {
-          case 0:
-            setBackSpaceCount(1);
-            break;
-          case 1:
-            setBackSpaceCount(2);
-            deletItem();
-        }
-        console.log(backSpaceCount);
-      }
-    }
+  const handleInputFocus = () => {
+    setSuggestionBoxVisible(true);
+    clearTimeout(blurTimeoutRef.current);
   };
-  const deletItem = () => {
-    console.log("i ran");
-    setTextArray((prev) => prev.filter((_, i) => i !== prev.length - 1));
-    setBackSpaceCount(0);
+  const handleInputBlur = () => {
+    blurTimeoutRef.current = setTimeout(() => {
+      setSuggestionBoxVisible(false);
+    }, 200);
   };
-  //   console.log(inputRef?.current?.innerText, textArray);
+
+  const handleSuggestionBoxFocus = () => {
+    clearTimeout(blurTimeoutRef.current);
+  };
+
   return (
     <div className="custom_input" onClick={focusInput}>
-      {textArray.map((t, i) => (
-        <span
-          className={`pill ${
-            backSpaceCount === 1 && i === textArray.length - 1
-              ? " highlight"
-              : ""
-          }`}
-        >
-          {/* <img /> */}
-          {t}
-        </span>
+      {selectedUserArray.map((user, i) => (
+        <Pill
+          key={user.id}
+          backSpaceCount={backSpaceCount}
+          selectedUserArray={selectedUserArray}
+          user={user}
+          deleteUser={deleteUser}
+          index={i}
+        />
       ))}
       <span className="wrapper">
         <span
@@ -65,10 +121,21 @@ export const Input = () => {
           contentEditable
           className="input"
           role="textbox"
-          value={"value"}
+          placeholder={placeholder}
+          onInput={handleInput}
           onKeyDown={handleKey}
+          onFocus={handleInputFocus}
+          onBlur={handleInputBlur}
         />
-        <div className="suggestion_box"></div>
+        {suggestionBoxVisible && (
+          <div
+            className="suggestion_box"
+            onFocus={handleSuggestionBoxFocus}
+            onBlur={handleInputBlur}
+          >
+            <UserList users={filteredData} selectUser={selectUser} />
+          </div>
+        )}
       </span>
     </div>
   );
