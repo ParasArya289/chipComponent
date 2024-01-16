31f17c588b62fce8617f3ef61c544748661a96eb
Paras Arya <parasarya289@gmail.com>
2024-01-16 23:42:09 +0530

    feat: zepto assignment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import { Input } from "./Components/Input/Input";
+import userData from "./userData";
 
 function App() {
   return (
     <div className="App">
       <h2>Zepto</h2>
-      <Input />
+      <Input data={userData} placeholder="Add new user"/>
     </div>
   );
 }
