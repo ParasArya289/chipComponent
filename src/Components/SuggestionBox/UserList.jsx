328b128dfe8ad5746ba205e1ce4ac4baae1f65e1
Paras Arya <parasarya289@gmail.com>
2024-01-16 23:41:53 +0530

    feat: UserList component

---
+++ src/Components/SuggestionBox/UserList.jsx
@@ -0,0 +1,15 @@
+import "./UserList.css";
+
+export const UserList = ({ users, selectUser }) => {
+  return users.map((user) => (
+    <button key={user.id} className="user" onClick={() => selectUser(user)}>
+      <div className="user-info">
+        <div className="user-column">
+          <img src={user.profilePic} height="30" width="30" alt="Profile" />
+          <span>{user.name}</span>
+        </div>
+        <span className="user-email">{user.email}</span>
+      </div>
+    </button>
+  ));
+};
