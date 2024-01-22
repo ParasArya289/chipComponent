02e98e0f1b663b9e377259671f4018bd4bdefb28
Paras Arya <parasarya289@gmail.com>
2024-01-23 02:56:08 +0530

    feat: props setting for Input component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 import { Input } from "./Components/Input/Input";
 import userData from "./userData";
 
 function App() {
+  const [selectedUserArray, setSelectedUserArray] = useState([]);
+
   return (
     <div className="App">
       <h2>Pick User</h2>
-      <Input data={userData} placeholder="Add new user"/>
+      <Input
+        data={userData}
+        selectedUserArray={selectedUserArray}
+        setSelectedUserArray={setSelectedUserArray}
+        placeholder="Add new user"
+      />
     </div>
   );
 }
