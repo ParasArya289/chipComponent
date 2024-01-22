0ae9b6d9ae38515e3bc8b86e7b486974cb82082f
Paras Arya <parasarya289@gmail.com>
2024-01-23 03:07:35 +0530

    fix: tab index for safari

--- src/Components/SuggestionBox/UserList.jsx
+++ src/Components/SuggestionBox/UserList.jsx
@@ -2,7 +2,12 @@ import "./UserList.css";
 
 export const UserList = ({ users, selectUser }) => {
   return users.map((user) => (
-    <button key={user.id} className="user" onClick={() => selectUser(user)}>
+    <button
+      tabIndex="0"
+      key={user.id}
+      className="user"
+      onClick={() => selectUser(user)}
+    >
       <div className="user-info">
         <div className="user-column">
           <img src={user.profilePic} height="30" width="30" alt="Profile" />
